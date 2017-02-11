ea4263a3a2b1dffa85a24f405ab1ecab8167ff40
axeclbr <axeclbr@posteo.de>
2017-02-11 22:20:54 +0100

    Refactor build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-GIT_SHA=`git rev-parse --short HEAD`
-BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-VERSION=`cat VERSION`
-
 docker build \
-  --build-arg VCS_REF=$GIT_SHA \
-  --build-arg BUILD_DATE=$BUILD_DATE \
-  --build-arg VERSION=$VERSION \
+  --build-arg VCS_REF=`git rev-parse --short HEAD` \
+  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+  --build-arg VERSION=`cat VERSION` \
   -t axeclbr/git .
